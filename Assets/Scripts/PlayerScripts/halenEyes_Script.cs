06e1cf45ab8f7d7f10002cd3268b4eb52c4c3272
daviddunkelman <dunkelmand@gmail.com>
2017-02-06 10:21:33 -0500

    Halen Eye System
    put in place system to change halen expresssion

---
+++ Assets/Scripts/PlayerScripts/halenEyes_Script.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class halenEyes_Script : MonoBehaviour {
+
+
+
+	public Texture2D[] Eyes = new Texture2D[14];
+	/*
+	0 - squint
+	1 - confused
+	2 - annoyed
+	3 - unimpressed 2
+	4 - unimpressed
+	5 - unimpressed "tapper"
+	6 - happy
+	7 - sad
+	8 - anger 2
+	9 - anger
+	10 - surprised 2
+	11 - surprised 1
+	12 - surprised 
+	13 - neutral
+	*/
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		//This is what it looks like to change the expression. Just specify what expression needed
+		//GetComponent<MeshRenderer> ().material.mainTexture = Eyes [0];
+			
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
