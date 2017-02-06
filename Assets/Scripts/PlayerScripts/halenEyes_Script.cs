2c5e965583074d1372c44b0cdf171a819dbcb38e
daviddunkelman <dunkelmand@gmail.com>
2017-02-06 14:00:48 -0500

    halen Eye script

--- Assets/Scripts/PlayerScripts/halenEyes_Script.cs
+++ Assets/Scripts/PlayerScripts/halenEyes_Script.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class halenEyes_Script : MonoBehaviour {
 	public static bool finished = true;
 	public Texture2D[] Eyes = new Texture2D[14];
+	bool blink = true;
+	bool running = false;
 	[System.Serializable]
 	public struct EyeStruct
 	{
@@ -38,7 +40,10 @@ public class halenEyes_Script : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (blink && !running) {
+			float delay = Random.Range (3f, 8f);
+			StartCoroutine (Blink (delay));
+		}
 		//StartCoroutine (EyeExpression (0, 2,true));
 
 	}
@@ -46,21 +51,32 @@ public class halenEyes_Script : MonoBehaviour {
 	public IEnumerator EyeExpression(int EyeIndex, float Duration, bool ReturntoNeutral = true, EyeStruct[] e = null, int i = 0)
 	{
 		GetComponent<MeshRenderer> ().material.mainTexture = Eyes [EyeIndex];
-		//finished = false;
 		yield return new WaitForSeconds (Duration);
 		if (ReturntoNeutral ==true) {	
 			GetComponent<MeshRenderer> ().material.mainTexture = Eyes [13];
 		}
 		if(e != null)
 			RunSequence (e, ++i); 
-		//finished = true;
+		
 	}
 
 	public void RunSequence(EyeStruct[] e, int i = 0)
 	{
 		//if (halenEyes_Script.finished) {
-		if(i < e.Length)
-			StartCoroutine(EyeExpression(e[i].index,e[i].duration, i == e.Length - 1, e, i));
+		if (i < e.Length) {
+			running = true;
+			StartCoroutine (EyeExpression (e [i].index, e [i].duration, i == e.Length - 1, e, i));
+		}
+		else
+			running = false;
 		//}
+	}
+
+	IEnumerator Blink(float delay)
+	{
+		blink = false;
+		yield return new WaitForSeconds (delay);
+		StartCoroutine(EyeExpression (0, 0.05f));
+		blink = true;
 	}
 }
