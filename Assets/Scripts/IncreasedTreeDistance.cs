90e708b10f8e4a1445a484aca4edecc6d2766b38
daviddunkelman <DavidLee3@cunet.carleton.ca>
2017-02-23 18:43:43 -0500

    Cinematics - ShipLanding

---
+++ Assets/Scripts/IncreasedTreeDistance.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IncreasedTreeDistance : MonoBehaviour {
+
+	public float distance;
+	public Terrain terrain;
+
+	void Start () {
+		terrain.treeDistance = distance;
+	}
+}
