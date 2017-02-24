600c533a507d21c22cf596847fe19c86fe96483a
daviddunkelman <dunkelmand@gmail.com>
2017-02-23 20:35:38 -0500

    cinemartic

--- Assets/Scripts/PlayerScripts/CameraSequence.cs
+++ Assets/Scripts/PlayerScripts/CameraSequence.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraSequence : MonoBehaviour {
-
+	[System.Serializable]
 	public struct CameraStruct
 	{
 		public GameObject Camera;
@@ -13,16 +13,38 @@ public class CameraSequence : MonoBehaviour {
 
 	public CameraStruct[] CamStep;
 
+	int index = 0;
+	bool goTime;
 
-
-
+	float Timer;
 	// Use this for initialization
 	void Start () {
-		
+		goTime = true;
 	}
+
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (goTime) {
+			
+			CamStep [index].Camera.SetActive (true);
+			CamStep[index].Subject.SetActive (true);
+			if (index != 0) {
+				CamStep [index-1].Camera.SetActive (false);
+				CamStep [index-1].Subject.SetActive (false);
+			}
+			goTime = false;
+			Timer = 0;
+
+		}
+		else {
+			if (Timer >= CamStep [index].duration) {
+				index++;
+				goTime = true;
+			} else {
+				Timer += Time.deltaTime; 
+			}
+		}
+
 	}
 }
