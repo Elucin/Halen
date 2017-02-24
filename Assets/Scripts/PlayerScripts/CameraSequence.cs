f8dc13a698ec97a4dd1273f096d3b5241e9470eb
daviddunkelman <dunkelmand@gmail.com>
2017-02-23 20:12:28 -0500

    cine

---
+++ Assets/Scripts/PlayerScripts/CameraSequence.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSequence : MonoBehaviour {
+
+	public struct CameraStruct
+	{
+		public GameObject Camera;
+		public float duration;
+		public GameObject Subject;
+	};
+
+	public CameraStruct[] CamStep;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
