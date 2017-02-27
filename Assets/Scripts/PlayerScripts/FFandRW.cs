8d15bb530783a04c35366b5f3d722e7c5d98de2b
daviddunkelman <dunkelmand@gmail.com>
2017-02-27 16:06:55 -0500

    trust in progress

---
+++ Assets/Scripts/PlayerScripts/FFandRW.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FFandRW : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.LeftArrow)&&Time.timeScale>0.1f)
+			Time.timeScale -= 0.1f;
+		if (Input.GetKey (KeyCode.RightArrow))
+			Time.timeScale += 0.1f;
+		if (Input.GetKey (KeyCode.DownArrow))
+			Time.timeScale =1;
+		
+	}
+
+
+}
