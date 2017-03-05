b2d114a31223a6f1de9798f002e06bfd3cc37d71
Dunkma <dunkelmand@gmail.com>
2017-03-05 17:41:20 -0500

    trust progress + simple move script

---
+++ Assets/simpleMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class simpleMove : MonoBehaviour {
+	public Transform startPoint;
+	public Transform endPoint;
+	public float duration;
+	float progress;
+	float counter;
+
+
+
+	// Use this for initialization
+	void Start () {
+		counter = 0;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		progress = counter/duration;
+
+		this.transform.position = Vector3.Lerp (startPoint.position, endPoint.position, progress);
+		this.transform.rotation = Quaternion.Lerp (startPoint.rotation, endPoint.rotation, progress);
+		counter +=Time.deltaTime;
+		if (counter > duration) {
+			Destroy (this);
+		}
+	
+	}
+
+
+}
