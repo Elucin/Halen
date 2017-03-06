9b49f9dfc7e3373e0f927b4c4961ad89b20b42e6
daviddunkelman <dunkelmand@gmail.com>
2017-03-06 15:21:22 -0500

    improved cutscene

---
+++ Assets/disableTime.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class disableTime : MonoBehaviour {
+
+	public float duration;
+
+	// Use this for initialization
+	void Start () {
+		if (duration == null) {
+			gameObject.SetActive (false); 
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		duration -= Time.deltaTime;
+		if (duration <= 0) {
+			gameObject.SetActive (false); 
+		}
+	}
+}
