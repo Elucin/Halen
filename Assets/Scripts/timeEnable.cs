db53aedf335aff1f03fa485375b6873397d06b45
daviddunkelman <dunkelmand@gmail.com>
2017-03-08 17:34:04 -0500

    adding 2arm to cutscene

--- Assets/Scripts/timeEnable.cs
+++ Assets/Scripts/timeEnable.cs
@@ -7,13 +7,20 @@ public class timeEnable : MonoBehaviour {
 	public float duration;
 	public GameObject[] Object;
 	int amount;
+	public GameObject[] Hidden;
+	int amount2;
 
 	// Use this for initialization
 	void Start () {
+		
 		amount = Object.Length;
+		amount2 = Hidden.Length;
 		if (duration == null) {
 			for (int i = 0; i < amount; i++) {
 				Object[i].SetActive (true); 
+			}
+			for (int j = 0; j < amount2; j++) {
+				Hidden[j].SetActive (false); 
 			}
 
 			Destroy (this);
@@ -26,6 +33,9 @@ public class timeEnable : MonoBehaviour {
 		if (duration <= 0) {
 			for (int i = 0; i < amount; i++) {
 				Object[i].SetActive (true); 
+			}
+			for (int j = 0; j < amount2; j++) {
+				Hidden[j].SetActive (false); 
 			}
 			Destroy (this);
 		}
