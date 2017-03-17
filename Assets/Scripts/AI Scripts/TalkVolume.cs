2f3116e245eb0baf3537108bac3f16add90bf927
daviddunkelman <dunkelmand@gmail.com>
2017-03-17 13:34:26 -0400

    Random Carg Talking

---
+++ Assets/Scripts/AI Scripts/TalkVolume.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TalkVolume : MonoBehaviour {
+
+	public AudioSource Voice;
+	float randomDuration;
+	int randomizer;
+
+	// Use this for initialization
+	void Start () {
+		Voice.volume = 0f;
+		randomDuration = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (randomDuration <= 0) {
+			Voice.volume = 0f;
+			randomizer = Random.Range (0, 4);
+			if (randomizer == 0) {
+				Voice.volume = 1f;
+			}
+			randomDuration = Random.Range (1, 6);
+		} else {
+			randomDuration -= Time.deltaTime;
+		}
+	}
+}
