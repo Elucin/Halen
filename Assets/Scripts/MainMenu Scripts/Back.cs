eec26d03919212d018cabec771792aaddf173f09
Andrew Smith <ajsmith22@live.com>
2017-03-20 15:43:10 -0400

    Cancel Button In Menu

---
+++ Assets/Scripts/MainMenu Scripts/Back.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(UnityEngine.UI.Button))]
+public class Back : MonoBehaviour, IPointerEnterHandler
+{
+    public GameObject optionsMenu;
+    public GameObject MainMenu;
+    bool selected;
+    EventSystem es;
+
+	// Use this for initialization
+	void Start () {
+        //optionsMenu = GameObject.Find("settings_panel");
+        //MainMenu = GameObject.Find("mainMenu_panel");
+        es = GameObject.FindObjectOfType<EventSystem>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (es.currentSelectedGameObject == gameObject)
+            {
+                Debug.Log(name);
+                optionsMenu.transform.root.GetComponent<OptionsMenu>().Cancel();
+                optionsMenu.SetActive(false);
+                MainMenu.SetActive(true);
+            }
+        }
+	}
+
+    public void OnPointerEnter(PointerEventData p)
+    {
+        selected = true;
+    }
+
+    public void OnPointerExit(PointerEventData p)
+    {
+        selected = false;
+    }
+}
--- Assets/Scripts/MainMenu Scripts/selectOnInput.cs
+++ Assets/Scripts/MainMenu Scripts/selectOnInput.cs
@@ -17,7 +17,7 @@ public class selectOnInput : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetAxisRaw("Vertical") != 0 && buttonSelected == false)
+		if((Input.GetAxisRaw("Vertical") != 0 || Input.GetAxisRaw("Horizontal") != 0) && buttonSelected == false)
         {
             eventSystem.SetSelectedGameObject(selectedObject);
             buttonSelected = true;
