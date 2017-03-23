8c0e5c332244a42628cd82a85f38fa3fb60f9792
daviddunkelman <DavidDunkelman@cunet.carleton.ca>
2017-03-23 10:51:14 -0400

    replaced charger tex and chromatic aberration

---
+++ Assets/Scripts/CameraChromaticAberration.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.ImageEffects;
+
+public class CameraChromaticAberration : MonoBehaviour {
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.GetComponent<VignetteAndChromaticAberration> ().chromaticAberration = (100-PlayerControl.Health )/4;
+	}
+}
