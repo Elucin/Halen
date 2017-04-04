4ef8ba38dc5b88fcd90824d9e33c7eae40be9ea9
Andrew Smith <ajsmith22@live.com>
2017-04-04 11:13:36 -0400

    Score Stuff

--- Assets/Scripts/LevelScripts/HSController.cs
+++ Assets/Scripts/LevelScripts/HSController.cs
@@ -6,8 +6,8 @@ public class HSController : MonoBehaviour
     private string secretKey = "589811"; // Edit this value and make sure it's the same as the one stored on the server
     //public string addScoreURL = "ug.csit.carleton.ca/~andrewsmith4/Halen/addscore.php?"; //be sure to add a ? to your url
     //public string highscoreURL = "ug.csit.carleton.ca/~andrewsmith4/Halen/display.php";
-    public string addScoreURL = "http://www.skypyre.com/HalenScore/addscore.php?"; //be sure to add a ? to your url
-    public string highscoreURL = "http://www.skypyre.com/HalenScore/display.php";
+    public string addScoreURL = "https://www.skypyre.com/scripts/HalenScore/addscore.php?"; //be sure to add a ? to your url
+    public string highscoreURL = "https://www.skypyre.com/scripts/HalenScore/display.php";
 
     void Start()
     {
@@ -37,17 +37,20 @@ public class HSController : MonoBehaviour
     // remember to use StartCoroutine when calling this function!
     IEnumerator GetScores()
     {
-        gameObject.GetComponent<GUIText>().text = "Loading Scores";
-        WWW hs_get = new WWW(highscoreURL);
-        yield return hs_get;
+        if (GetComponent<UnityEngine.UI.Text>() != null)
+        {
+            gameObject.GetComponent<UnityEngine.UI.Text>().text = "Loading Scores";
+            WWW hs_get = new WWW(highscoreURL);
+            yield return hs_get;
 
-        if (hs_get.error != null)
-        {
-            print("There was an error getting the high score: " + hs_get.error);
-        }
-        else
-        {
-            gameObject.GetComponent<GUIText>().text = hs_get.text; // this is a GUIText that will display the scores in game.
+            if (hs_get.error != null)
+            {
+                print("There was an error getting the high score: " + hs_get.error);
+            }
+            else
+            {
+                gameObject.GetComponent<UnityEngine.UI.Text>().text = hs_get.text; // this is a GUIText that will display the scores in game.
+            }
         }
     }
 
