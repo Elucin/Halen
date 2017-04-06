9b3053735ac38a62c21c6b0bd4491a4805f4ccdb
daviddunkelman <dunkelmand@gmail.com>
2017-04-06 09:51:20 -0400

    c1c fix, attempts at theravall

---
+++ Assets/Camera_Track.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Track : MonoBehaviour {
+
+	public float xSpeed;
+	public float ySpeed;
+	public float zSpeed;
+	public float Duration;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Duration > 0) {
+			Duration -= Time.deltaTime;
+			this.transform.localPosition+= new Vector3(xSpeed*Time.deltaTime, ySpeed*Time.deltaTime, zSpeed*Time.deltaTime);
+
+		}
+	}
+}
