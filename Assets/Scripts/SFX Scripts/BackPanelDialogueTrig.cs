d69b362e8134e32d78029b63030399fee7059a62
daviddunkelman <dunkelmand@gmail.com>
2017-04-07 15:47:20 -0400

    Improved Charger Kill Tutorial

---
+++ Assets/Scripts/SFX Scripts/BackPanelDialogueTrig.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackPanelDialogueTrig : MonoBehaviour {
+
+	public AudioClip Dialogue;
+
+	public AudioSource CurrentSound;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.transform.tag == "Enemy")
+		{
+			CurrentSound.PlayOneShot (Dialogue, 1);
+		}
+	}
+}
