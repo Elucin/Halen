b2f2e8a6ab9085012bbdc006dde45c116009e22a
mattvlog <mattvlog123@gmail.com>
2017-04-06 20:46:03 -0400

    credits can play in menu

--- Assets/Video/PlayVideoMainMenu.cs
+++ Assets/Video/PlayVideoMainMenu.cs
@@ -4,7 +4,8 @@ using System.Collections.Generic;
 
 public class PlayVideoMainMenu : MonoBehaviour {
     public static int clipIndex = 0;
-
+    bool playingCredits = false;
+    public GameObject menu;
     [System.Serializable]
     public struct av
     {
@@ -19,12 +20,41 @@ public class PlayVideoMainMenu : MonoBehaviour {
     public List<av> CutsceneList = new List<av>();
     // Use this for initialization
     void Start () {
+        PlayBackground();
+
+    }
+
+    public void PlayCredits()
+    {
+        menu.SetActive(false);
         Renderer r = GetComponent<Renderer>();
         AudioSource a = GameObject.FindObjectOfType<AudioSource>();
-        r.material = CutsceneList[clipIndex].video;
-        a.clip = CutsceneList[clipIndex].audio;
+        r.material = CutsceneList[1].video;
+        a.clip = CutsceneList[1].audio;
+        movie = (MovieTexture)r.material.mainTexture;
+        movie.Play();
+        playingCredits = true;
+    }
+
+    void PlayBackground()
+    {
+        Renderer r = GetComponent<Renderer>();
+        AudioSource a = GameObject.FindObjectOfType<AudioSource>();
+        r.material = CutsceneList[0].video;
+        a.clip = CutsceneList[0].audio;
         movie = (MovieTexture)r.material.mainTexture;
         movie.Play();
         movie.loop = true;
-	}
+    }
+
+    void Update()
+    {
+        if((playingCredits && !movie.isPlaying) || Input.GetButtonDown("Cancel"))
+        {
+            movie.Stop();
+            menu.SetActive(true);
+            PlayBackground();
+            playingCredits = false;
+        }
+    }
 }
